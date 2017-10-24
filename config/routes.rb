3ddea0f8874679656bd84c0bcf3d5a2fdcc1678a
rouf049 <abdur.rouf049@gmail.com>
2017-10-24 15:47:35 +0700

    Finish stock model video

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/my_portfolio'
+
   root 'welcome#index'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'my_portfolio', to: 'users#my_portfolio'
+  
 end
