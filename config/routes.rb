ea9104afbc35579bc488ea10ebcb18dabc71b395
rouf049 <abdur.rouf049@gmail.com>
2017-10-26 12:13:12 +0700

    Stock controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'stock/search'
+
   get 'users/my_portfolio'
 
   root 'welcome#index'
@@ -6,5 +8,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'my_portfolio', to: 'users#my_portfolio'
+
+  get 'search_stocks', to: 'stock#search'
   
 end
